b7f7012f7e47ba66eb90e83f84d0d52363c3e6d9
jseb22 <gsbs4982@gmail.com>
2025-02-09 00:02:50 -0500

    solucion ejericicio 6, sebastianramirez

---
+++ ejercicio6.py
@@ -0,0 +1,5 @@
+def encontrar_max_min():
+    numeros = list(map(int, input("Ingrese una lista de números separados por espacios: ").split()))
+    print(max(numeros), min(numeros))
+
+encontrar_max_min()
